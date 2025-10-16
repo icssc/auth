8c6771d0a7fcdd6125b9f6c0617ec006dc49c3bb
Kevin Wu <kevinwu098@gmail.com>
2025-10-16 03:42:13 -0700

    feat: init drizzle

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
+import { getDb } from "./db";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.get("/", (c) => {
+    const db = getDb(c.env.AUTH_DB);
+
     return c.text("Hello Hono!");
 });
 
